fbaac3cf948e5bc2bed0586009592916dbeb77d0
mini-1018 <anjaemin1018@gmail.com>
2025-01-15 13:06:50 +0900

    :sparkles: [기능구현] cors설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 모든 도메인 허용
+  app.enableCors();
+
+  // 또는 상세 설정
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://yourfrontend.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'multipart/form-data'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
