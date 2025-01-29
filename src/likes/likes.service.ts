bc32166d04bd8f7b1dc55d1a5ce823476ccc3748
mini-1018 <anjaemin1018@gmail.com>
2025-01-30 00:01:06 +0900

    :sparkles: [feat] 좋아요 기능

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { LikesRepository } from './likes.repository';
 
 @Injectable()
-export class LikesService {}
+export class LikesService {
+  constructor(private readonly likesRepository: LikesRepository) {}
+
+  async toggleLike(postId: number, userId: number) {
+    const existingLike = await this.likesRepository.findLike(userId, postId);
+    console.log(existingLike);
+    if (existingLike) {
+      return this.likesRepository.deleteLike(userId, postId);
+    }
+    return this.likesRepository.createLike(userId, postId);
+  }
+}
